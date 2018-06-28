c11647f4220b8b7ea76484e954c5eddd6948f687
Celine Low and Thiam Hock <developers+celine+thiamhock@siliconjungles.com>
2018-06-28 18:52:23 +0800

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EQRCode.MixProject do
   def project do
     [
       app: :eqrcode,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       name: "EQRCode",
