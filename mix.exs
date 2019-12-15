626d34570e874b2da17b614a7d8d1d90c229f0e0
Thiam Hock Ng <nthock@gmail.com>
2019-12-15 23:50:51 +0800

    bump version to 0.1.7

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EQRCode.MixProject do
   def project do
     [
       app: :eqrcode,
-      version: "0.1.6",
+      version: "0.1.7",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       name: "EQRCode",
