cde803e209eb4840750a24c394a0bb397117f96a
Thiam Hock Ng <nthock@gmail.com>
2021-05-05 15:31:05 +0800

    Bump version to 0.1.8 and update ex_doc dependency

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Simple QR Code Generator written in Elixir with no other dependencies.
 ```elixir
 def deps do
   [
-    {:eqrcode, "~> 0.1.7"}
+    {:eqrcode, "~> 0.1.8"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EQRCode.MixProject do
   def project do
     [
       app: :eqrcode,
-      version: "0.1.7",
+      version: "0.1.8",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       name: "EQRCode",
@@ -43,7 +43,7 @@ defmodule EQRCode.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false}
     ]
   end
 end
