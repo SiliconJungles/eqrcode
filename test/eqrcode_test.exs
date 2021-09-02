8ce379a07d260e74b88f0671b7b0ec4004e37c65
Thiam Hock Ng <nthock@gmail.com>
2021-09-02 23:42:38 +0800

    Add test for charlist error handling

--- test/eqrcode_test.exs
+++ test/eqrcode_test.exs
@@ -44,5 +44,13 @@ defmodule EQRCodeTest do
         long_qr_content |> EQRCode.encode()
       end
     end
+
+    test "should return error when the input is a charlist" do
+      charlist = 'this_is_a_charlist'
+
+      assert_raise ArgumentError, "You have passed a list instead of string. Did you pass in a charlist by mistake?", fn ->
+        charlist |> EQRCode.encode()
+      end
+    end
   end
 end
